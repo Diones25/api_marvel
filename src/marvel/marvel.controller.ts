fa08b715c6abda6a2c4b7b631b1ede83f617663e
Diones <pereiradiones987@gmail.com>
2025-03-02 20:29:53 -0300

    adicionando endpoints na controller

--- src/marvel/marvel.controller.ts
+++ src/marvel/marvel.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import { MarvelService } from './marvel.service';
 
 @Controller('marvel')
-export class MarvelController {}
+export class MarvelController {
+  constructor(private readonly marvelService: MarvelService) {}
+
+  @Get('character')
+  async getCharacter(@Query('name') name: string): Promise<any> {
+    if (!name) {
+      throw new BadRequestException('O parâmetro name é obrigatório');
+    }
+    return this.marvelService.getCharacter(name);
+  }
+
+  @Get('characters')
+  async getAllCharacters(): Promise<any> {
+    return this.marvelService.getAllCharacters();
+  }
+}
--- src/marvel/marvel.service.ts
+++ src/marvel/marvel.service.ts
@@ -7,6 +7,8 @@ export class MarvelService {
   private readonly publicKey = process.env.MARVEL_PUBLIC_KEY;
   private readonly privateKey = process.env.MARVEL_PRIVATE_KEY;
   private readonly baseUrl = 'https://gateway.marvel.com/v1/public';
+  private readonly timestamp = Date.now().toString();
+  private readonly hash = this.generateHash(this.timestamp);
 
   //Método para gerar o hash MD5
   private generateHash(timestamp: string): string {
@@ -17,15 +19,31 @@ export class MarvelService {
   }
 
   //Método para buscar personagens
-  async getCharacters(name: string): Promise<any> {
-    const timestamp = Date.now().toString();
-    const hash = this.generateHash(timestamp);
+  async getCharacter(name: string): Promise<any> {
 
     const params = {
       apikey: this.publicKey,
-      ts: timestamp,
-      hash: hash,
+      ts: this.timestamp,
+      hash: this.hash,
       name: name,
+    };
+
+    try {
+      const response = await axios.get(`${this.baseUrl}/characters`, {
+        params,
+      });
+      return response.data;
+    } catch (error) {
+      throw new Error(`Erro ao buscar personagens: ${error.message}`);
+    }
+  }
+
+  async getAllCharacters(): Promise<any> {
+
+    const params = {
+      apikey: this.publicKey,
+      ts: this.timestamp,
+      hash: this.hash
     };
 
     try {
